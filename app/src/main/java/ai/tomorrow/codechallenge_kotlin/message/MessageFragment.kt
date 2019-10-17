1182a2532aee19b2759f7f345ed299d9cf2fba67
Yini <ydxb7@mail.missouri.edu>
2019-10-17 02:43:33 -0700

    finish get different type messages

--- app/src/main/java/ai/tomorrow/codechallenge_kotlin/message/MessageFragment.kt
+++ app/src/main/java/ai/tomorrow/codechallenge_kotlin/message/MessageFragment.kt
@@ -2,6 +2,7 @@ package ai.tomorrow.codechallenge_kotlin.message
 
 import ai.tomorrow.codechallenge_kotlin.adapter.MessageRecyclerViewAdapter
 import ai.tomorrow.codechallenge_kotlin.databinding.MessageFragmentBinding
+import ai.tomorrow.codechallenge_kotlin.model.DatabaseMessage
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -32,8 +33,7 @@ class MessageFragment : Fragment() {
 
 
         binding.settingIv.setOnClickListener {
-            val settingDialogFragment =
-                SettingDialogFragment()
+            val settingDialogFragment = SettingDialogFragment()
             settingDialogFragment.show(
                 requireNotNull(fragmentManager),
                 SettingDialogFragment::class.simpleName
@@ -52,37 +52,39 @@ class MessageFragment : Fragment() {
         return binding.root
     }
 
+    private val observer = Observer<List<DatabaseMessage>> { adapter.insertNewData(it) }
+
     private fun showAllMessages() {
+        viewModel.messagesFriend.removeObserver(observer)
+        viewModel.messagesNoFriend.removeObserver(observer)
         if (viewModel.messagesAll.value != null) {
             adapter.setData(requireNotNull(viewModel.messagesAll.value))
         } else {
             adapter.setData(ArrayList())
         }
-        viewModel.messagesAll.observe(this, Observer {
-            adapter.insertNewData(it)
-        })
+        viewModel.messagesAll.observe(this, observer)
     }
 
     private fun showFriendMessages() {
+        viewModel.messagesAll.removeObserver(observer)
+        viewModel.messagesNoFriend.removeObserver(observer)
         if (viewModel.messagesFriend.value != null) {
             adapter.setData(requireNotNull(viewModel.messagesFriend.value))
         } else {
             adapter.setData(ArrayList())
         }
 
-        viewModel.messagesFriend.observe(this, Observer {
-            adapter.insertNewData(it)
-        })
+        viewModel.messagesFriend.observe(this, observer)
     }
 
     private fun showNotFriendMessages() {
+        viewModel.messagesAll.removeObserver(observer)
+        viewModel.messagesFriend.removeObserver(observer)
         if (viewModel.messagesNoFriend.value != null) {
             adapter.setData(requireNotNull(viewModel.messagesNoFriend.value))
         } else {
             adapter.setData(ArrayList())
         }
-        viewModel.messagesNoFriend.observe(this, Observer {
-            adapter.insertNewData(it)
-        })
+        viewModel.messagesNoFriend.observe(this, observer)
     }
 }
